2677aed4ff9c4f1a4a594cae6114718dd2147e8d
hbyyy <qjaduddl94@gmail.com>
2020-04-17 20:54:48 +0900

    feat: web에서 facebook social login 구ㅕㅎㄴ

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from members.views import index
+from members.views import index, hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('oauth/', index, name='index'),
+    path('test/', hello, name='hello')
 ]
 
 urlpatterns_api = [
--- members/views.py
+++ members/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render
 
 
@@ -5,4 +6,28 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'members/index.html')
+    context = {
+        'client_id': '935204026920614',
+    }
+    return render(request, 'members/index.html', context)
+
+
+def hello(request):
+    code = request.GET['code']
+    print(code)
+    params = {
+        'client_id': '935204026920614',
+        'redirect_uri': "http://localhost:8000/test/",
+        'client_secret': "b7c004f382e0a999ecf4c1e119645dd7",
+        'code': code
+    }
+
+    url = "https://graph.facebook.com/v6.0/oauth/access_token"
+    request_url = "{url}?{params}".format(
+        url=url,
+        params='&'.join([f'{key}={value}' for key, value in params.items()])
+    )
+    print(request_url)
+    response = requests.get(request_url)
+    print(response.text)
+    return render(request, 'members/hello.html', context={'response': response.text})
