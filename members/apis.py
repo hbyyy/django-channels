36d1f6fc6b86fa81bb914574dc957187cddfffa9
hbyyy <qjaduddl94@gmail.com>
2020-04-18 00:02:20 +0900

    feat: drfso2 사용해 사용자 인증

--- members/apis.py
+++ members/apis.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from members.serializers import UserSerializer
 
@@ -14,6 +16,13 @@ class UserCreateView(generics.CreateAPIView):
 
 
 class UserRetrieveView(generics.RetrieveAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserAuthTestAPIView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
--- members/urls.py
+++ members/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from members.apis import UserCreateView, UserRetrieveView
+from members.apis import UserCreateView, UserRetrieveView, UserAuthTestAPIView
 
 urlpatterns = [
     path('', UserCreateView.as_view()),
     path('<int:pk>/', UserRetrieveView.as_view()),
+    path('test/', UserAuthTestAPIView.as_view())
 
 ]
